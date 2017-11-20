a0c7e7175150cd47cee9aab7c9414d19aad6a7c4
Alain Boudreault <alain.boudreault@me.com>
2017-11-20 18:59:46 -0500

    suppression de la clé

--- APIRiotGame-LeagueOfLegends/Ressources/LesGlobales.swift
+++ APIRiotGame-LeagueOfLegends/Ressources/LesGlobales.swift
@@ -11,6 +11,6 @@ import Foundation
 // https://na1.api.riotgames.com/lol/static-data/v3/champions?locale=en_US&tags=image&tags=info&tags=keys&tags=skins&tags=spells&tags=stats&tags=tags&dataById=false&api_key=RGAPI-5c33de81-6320-44c0-b5b7-7f27d1d98c41
 
 let RIOT_API = "https://na1.api.riotgames.com/lol/static-data/v3/champions?locale=en_US&tags=image&tags=info&tags=keys&tags=skins&tags=spells&tags=stats&tags=tags&dataById=false&api_key="
-let RIOT_API_KEY = "RGAPI-5c33de81-6320-44c0-b5b7-7f27d1d98c41"
+let RIOT_API_KEY = "insert your key here"
 let RIOT_API_IMAGE_URL = "https://ddragon.leagueoflegends.com/cdn/7.10.1/img/champion/"
 let RIOT_API_MAX_REQUEST = 20
